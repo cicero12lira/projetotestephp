20e6992e32189881995a8d355e2f831f1dfe38a7
cicero12lira <ciceroprr12@gmail.com>
2025-10-28 14:03:52 -0300

    banco

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
+$host = "testeserverphpgdl.mysql.database.azure.com";
+$user = "userphp";
+$password = "C144@top";
 $dbname = "teste";
 
 // Criar conexão
